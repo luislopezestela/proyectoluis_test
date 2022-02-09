07da6f13e2a840af86c9464a9dda4ccdcca063e7
Luis lopez estela <jose_1.9.96@hotmail.com>
2022-02-09 17:31:14 -0500

    Update luis_lopez.php

--- datos/modulos/instalar/paginas/index/luis_lopez.php
+++ datos/modulos/instalar/paginas/index/luis_lopez.php
@@ -4,3 +4,7 @@
 <h1 class="welcome_title_sty">Bienvenido</h1>
 </div>
 
+<div class="contenedor_dta">
+  <input type="text" name="" placeholder="Nombre de pagina">
+  <input type="text" name="" placeholder="Titulo de la pagina">
+</div>
