e9715a5649883cd5df624467dc95492fc7d54d0e
Luis lopez estela <jose_1.9.96@hotmail.com>
2022-02-25 00:59:47 -0500

    Create luis_lopez.php

---
+++ datos/modulos/bienvenido/paginas/bienvenido/luis_lopez.php
@@ -0,0 +1,9 @@
+<div class="contenido_dark">
+	<div class="text"><?=Luis::lang("bienvenido");?></div>
+	<div class="select_language">
+		<p class="alert_message_display"><?=Luis::lang("todos_los_datos_estan_protegidos_los_datos_seran_verificados_en_tiempo_real_verificacion_de_identidad");?></p>
+		<h3 class="alert_message_display_rep"><?=Luis::lang("los_datos_falsos_seran_verificados_y_reportados");?></h3>
+		<br>
+		<div class="btn btn__secondary start_page_data"><p><?=Luis::lang("iniciar");?></p></div>
+	</div>
+</div>
