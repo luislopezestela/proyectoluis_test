080c99849e9a0522eadfeeeab55e9b5691014193
Luis lopez estela <jose_1.9.96@hotmail.com>
2022-02-25 11:49:34 -0500

    Update principal.php

--- datos/modulos/bienvenido/paginas/principal.php
+++ datos/modulos/bienvenido/paginas/principal.php
@@ -2,8 +2,29 @@
 // https://localhost/
 $pagebase = Luis::dato("luis_base")->valor;
 if($pagebase):
+if($_SERVER["HTTPS"] != "on"){
+    header("Location: https://".$_SERVER["HTTP_HOST"]);
+    exit();
+}
+$base_pg="https://".$_SERVER["HTTP_HOST"]."/";
 ?>
-////
+<!DOCTYPE html>
+<html>
+<head>
+	<title id="title_pages"><?=Luis::lang("bienvenido");?></title>
+	<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="theme-color" content="#2c3e50">
+	
+	<link rel="shortcut icon" href="<?=$base_pg."datos/source/icons/shield.png";?>">
+	<link rel="stylesheet" type="text/css" href="<?=$base_pg."admin/datos/source/estilos/estilo.css";?>">
+	<link rel="stylesheet" type="text/css" href="<?=$base_pg."datos/modulos/bienvenido/source/style.css";?>">
+	 
+</head>
+<body id="body_luis_lopez">
+	<?=Vista::load("bienvenido"); ?>
+</body>
+</html>
 <?php
 else:	
 if($_SERVER["HTTPS"] != "on"){
