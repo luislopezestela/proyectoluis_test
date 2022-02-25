c6545b3938f2f1dd0b8639c0470365e6ea88c29b
Luis lopez estela <jose_1.9.96@hotmail.com>
2022-02-25 11:49:24 -0500

    Update accion-luis.php

--- datos/modulos/bienvenido/accion/start_data/accion-luis.php
+++ datos/modulos/bienvenido/accion/start_data/accion-luis.php
@@ -1,3 +1,8 @@
 <?php
-echo $url = "https://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
+$url = "https://".$_SERVER["HTTP_HOST"];
+$ur = new Luis();
+$ur->noms = "luis_base";
+$ur->urldata = $url."/";
+$ur->guardar_url_base_page();
+echo $ur->urldata."admin/";
 ?>
