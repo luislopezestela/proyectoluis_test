6d5782d6f6534db249a89f2110536f0f8ccdc3d4
Luis lopez estela <jose_1.9.96@hotmail.com>
2022-03-02 23:30:03 +0100

    Update accion-luis.php

--- admin/datos/modulos/bienvenido/accion/start_data_page/accion-luis.php
+++ admin/datos/modulos/bienvenido/accion/start_data_page/accion-luis.php
@@ -10,6 +10,6 @@ $n->p_tema = $tema;
 $n->p_nombre = $nombre;
 $n->p_titulo = $titulo;
 $n->p_descripcion = $descripcion;
-$n->eslogan = $eslogan;
+$n->p_eslogan = $eslogan;
 //$n->guardar_datos_pagina();
 print_r($n);
