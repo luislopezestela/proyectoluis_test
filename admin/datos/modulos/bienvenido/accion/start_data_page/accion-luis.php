aab8373623926e987cd27b39f453cfe1da6a62ee
Luis lopez estela <jose_1.9.96@hotmail.com>
2022-03-04 00:09:47 +0100

    Update accion-luis.php

--- admin/datos/modulos/bienvenido/accion/start_data_page/accion-luis.php
+++ admin/datos/modulos/bienvenido/accion/start_data_page/accion-luis.php
@@ -1,15 +1,10 @@
 <?php
 $tema = $_POST['tema'];
-$nombre = $_POST['nombre'];
-$titulo = $_POST['titulo'];
-$descripcion = $_POST['descripcion'];
-$eslogan = $_POST['eslogan'];
 
 $n = new Luis();
-$n->p_tema = $tema;
-$n->p_nombre = $nombre;
-$n->p_titulo = $titulo;
-$n->p_descripcion = $descripcion;
-$n->p_eslogan = $eslogan;
-//$n->guardar_datos_pagina();
-print_r($n);
+foreach($_POST as $key => $value){
+	$n->guardar_datos_pagina($key,$value);
+}
+
+$n->skin = $tema;
+$n->guardar_tema_pagina();
