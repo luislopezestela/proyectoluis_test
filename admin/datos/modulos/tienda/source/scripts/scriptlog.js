1dce264aa6e9ce7307caff35bb853a1d66bc3d96
Luis lopez estela <jose_1.9.96@hotmail.com>
2022-03-04 23:43:57 +0100

    Update scriptlog.js

--- admin/datos/modulos/tienda/source/scripts/scriptlog.js
+++ admin/datos/modulos/tienda/source/scripts/scriptlog.js
@@ -1,4 +1,6 @@
 var urline=$("#ur_timeline").attr("data");
+var doc = $("input[name='dni']");
+
 $(document).ready(function(){
 	$(document).on("submit", "#logboxinit", function(e){
 		e.preventDefault();
@@ -18,5 +20,48 @@ $(document).ready(function(){
 			}
 		});
 	});
+
+	/// registrar usuario a
+	$(document).on("submit", "#logboxinitregister", function(e){
+		e.preventDefault();
+		var dataToSend = $("#logboxinitregister").serialize();
+		$.ajax({
+			url:urline+list_action()+"luis_new_us_page",
+			type:"POST",
+			data:dataToSend,
+			dataType:"json",
+			success: function(data){
+				console.log(data)
+				if(data.type){
+					alertexito(data.mensaje)
+					//recargar(600)
+				}else{
+					alertadvertencia(data.mensaje)
+				}
+			}
+		});
+	});
+
+	
 });
 
+
+//////data document 
+	$(document).on("keyup", "input[name='dni']", function(){
+		var datinp = doc.val().length;
+		if(datinp > 7){
+			console.log(datinp);
+			$.ajax({
+				url:urline+list_action()+"luis_consuls",
+				type:"POST",
+				data:{docs:doc.val()},
+				dataType:"json",
+				success: function(data){
+					$("input[name='nombres']").val(data.nombres);
+					$("input[name='apellido_paterno']").val(data.apellidoPaterno);
+					$("input[name='apellido_materno']").val(data.apellidoMaterno);
+				}
+			});
+		}
+		
+	});
