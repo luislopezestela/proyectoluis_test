29bb7bb0b282d8e6b3ac15fc3a56b2446825db7a
Luis lopez estela <jose_1.9.96@hotmail.com>
2022-03-07 20:25:09 +0100

    Update accion-luis.php

--- admin/datos/modulos/tienda/accion/luis_new_us_page/accion-luis.php
+++ admin/datos/modulos/tienda/accion/luis_new_us_page/accion-luis.php
@@ -1,22 +1,25 @@
 <?php
-$_POST["dni"];
-$_POST["nombres"];
-$_POST["apellido_paterno"];
-$_POST["apellido_materno"];
-$_POST["correo"];
-$_POST["pass"];
-$_POST["pass_ver"];
+$alphabeth ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ1234567890_-";
+$codigos = "";
+for($i=0;$i<11;$i++){
+    $codigos .= $alphabeth[rand(0,strlen($alphabeth)-1)];
+}
 
 $correos = DatosMails::check_email($_POST["correo"]);
+$contra = sha1(md5($_POST['pass']));
+$contra2 = sha1(md5($_POST['pass_ver']));
 if($correos){
-	if($_POST["pass"]==$_POST["pass_ver"]) {
+	if($contra==$contra2) {
 		$person = new Luis::user_administrador_principal();
-		$person->dni = 
-		$person->nombre = 
-		$person->apellido_paterno = 
-		$person->apellido_materno = 
-		$person->correo = 
-		$person->pass = 
+		$person->dni = $_POST["dni"];
+		$person->nombre = $_POST["nombres"];
+		$person->apellido_paterno = $_POST["apellido_paterno"];
+		$person->apellido_materno = $_POST["apellido_materno"];
+		$person->username = $_POST["nombres"].$codigos;
+		$person->correo = $_POST["correo"];
+		$person->pass = $contra;
+		$person->ukrr = DatosAdmin::poner_guion(strip_tags($_POST["nombres"]))."_".DatosAdmin::poner_guion(strip_tags($_POST["apellido_paterno"]))."_".DatosAdmin::poner_guion(strip_tags($_POST["apellido_materno"]));
+		$person->fecha = date('Y-m-d H:i:s');
 		$person->crear_super_usuario_admin();
 		echo json_encode(array("type" => 1,'mensaje' => "Genial"));
 	}else{
