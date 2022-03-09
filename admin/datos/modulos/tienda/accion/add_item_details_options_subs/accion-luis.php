23678bd5488e7b032f8e2888f8d3256068bf8259
Luis lopez estela <jose_1.9.96@hotmail.com>
2022-03-09 13:10:47 -0500

    Update accion-luis.php

--- admin/datos/modulos/tienda/accion/add_item_details_options_subs/accion-luis.php
+++ admin/datos/modulos/tienda/accion/add_item_details_options_subs/accion-luis.php
@@ -4,11 +4,11 @@ if($_POST['type_option']){
 	if($item_view){
 		$opciones_detail = DatosAdmin::ver_opciones_detalles_list_one_name($item_view->id,$_POST['opcion_det']);
 		if($opciones_detail){
-			echo json_encode(array('tipo' => 0, 'message' => "Ya existe el nombre."));
+			echo json_encode(array('tipo' => 0, 'message' => Luis::lang("ya_existe_el_nombre")));
 		}elseif($_POST['opcion_det']==""){
-			echo json_encode(array('tipo' => 2, 'message' => "Ingresa el nombre."));
+			echo json_encode(array('tipo' => 2, 'message' => Luis::lang("ingresa_el_nombre")));
 		}elseif($_POST['option_pr']==""){
-			echo json_encode(array('tipo' => 3, 'message' => "Ingrese un precio."));
+			echo json_encode(array('tipo' => 3, 'message' => Luis::lang("ingresa_un_precio")));
 		}else{
 			$det = new DatosAdmin();
 			$det->nombre = htmlentities($_POST['opcion_det']);
@@ -21,15 +21,15 @@ if($_POST['type_option']){
 			if(count($detalles_opciones)==1){
 			 	DatosAdmin::options_noes_principal($item_view->id);
 			 	DatosAdmin::options_aser_principal($s[1]);
-			 	echo json_encode(array('tipo' => 1, 'message' => "Agregado con exito.", 'line' => $s[1], "active" => 1));
+			 	echo json_encode(array('tipo' => 1, 'message' => Luis::lang("agregado_con_exito"), 'line' => $s[1], "active" => 1));
 			 }else{
-			 	echo json_encode(array('tipo' => 1, 'message' => "Agregado con exito.", 'line' => $s[1], "active" => 0));
+			 	echo json_encode(array('tipo' => 1, 'message' => Luis::lang("agregado_con_exito"), 'line' => $s[1], "active" => 0));
 			 }
 		}
 	}else{
-		echo json_encode(array('tipo' => 0, 'message' => "Un error en los datos."));
+		echo json_encode(array('tipo' => 0, 'message' => Luis::lang("un_error_en_los_datos")));
 	}
 }else{
-	echo json_encode(array('tipo' => 0, 'message' => "Error de datos."));
+	echo json_encode(array('tipo' => 0, 'message' => Luis::lang("error_de_datos")));
 	
 }
