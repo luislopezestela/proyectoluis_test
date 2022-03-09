a4eb700d3176955fbfdaa00e56ce7a8553497082
Luis lopez estela <jose_1.9.96@hotmail.com>
2022-03-09 13:10:55 -0500

    Update accion-luis.php

--- admin/datos/modulos/tienda/accion/aceptar_venta/accion-luis.php
+++ admin/datos/modulos/tienda/accion/aceptar_venta/accion-luis.php
@@ -5,4 +5,4 @@ $compra->estado_de_venta = 6;
 $compra->vendedor = $_SESSION['admin_id'];
 $compra->aceptar_la_compra_web_vendedor();
 
-echo json_encode(array('estado' => "exito", 'mensaje' => "Se movio a la lista de recibidos."));
+echo json_encode(array('estado' => "exito", 'mensaje' => Luis::lang("se_movio_a_la_lista_de_recibidos")));
