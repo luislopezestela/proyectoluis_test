3b91c5421a9bf8f562774f4fcf68368c2ac27ed1
Luis lopez estela <jose_1.9.96@hotmail.com>
2022-03-09 13:10:51 -0500

    Update accion-luis.php

--- admin/datos/modulos/tienda/accion/add_item_details/accion-luis.php
+++ admin/datos/modulos/tienda/accion/add_item_details/accion-luis.php
@@ -6,20 +6,20 @@ if($_POST['item']){
 	if($item_view){
 		$type_detail = DatosAdmin::ver_opciones_type_one_name_view($item_view->id,$_POST['dat']);
 		if($type_detail){
-			echo json_encode(array('tipo' => 0, 'message' => "Ya existe el nombre."));
+			echo json_encode(array('tipo' => 0, 'message' => Luis::lang("ya_existe_el_nombre")));
 		}elseif($_POST['dat']){
 			$det = new DatosAdmin();
 			$det->nombre = htmlentities($_POST['dat']);
 			$det->id_items = $item_view->id;
 			$s=$det->add_detalle_type();
-			echo json_encode(array('tipo' => 1, 'message' => "Agregado con exito.", 'line' => $s[1]));
+			echo json_encode(array('tipo' => 1, 'message' => Luis::lang("agregado_con_exito"), 'line' => $s[1]));
 		}else{
-			echo json_encode(array('tipo' => 0, 'message' => "El campo esta vacio."));
+			echo json_encode(array('tipo' => 0, 'message' => Luis::lang("el_campo_esta_vacio")));
 		}
 	}else{
-		echo json_encode(array('tipo' => 0, 'message' => "Un error en los datos."));
+		echo json_encode(array('tipo' => 0, 'message' => Luis::lang("un_error_en_los_datos")));
 	}
 }else{
-	echo json_encode(array('tipo' => 0, 'message' => "Error de datos."));
+	echo json_encode(array('tipo' => 0, 'message' => Luis::lang("error_de_datos")));
 	
 }
