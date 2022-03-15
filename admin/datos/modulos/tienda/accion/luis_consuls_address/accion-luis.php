54205baa764af9b82fb637c6def2785d548279e8
Luis lopez estela <jose_1.9.96@hotmail.com>
2022-03-15 17:03:46 -0500

    Update accion-luis.php

--- admin/datos/modulos/tienda/accion/luis_consuls_address/accion-luis.php
+++ admin/datos/modulos/tienda/accion/luis_consuls_address/accion-luis.php
@@ -1,38 +1,17 @@
 <?php
-function getGeocodeData($address) {
-$address = urlencode($address);
-$googleMapUrl = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key=AIzaSyAzVClbbKRZ2Id-N8Xr-Sws5Z32NpVB-JY";
-$geocodeResponseData = file_get_contents($googleMapUrl);
-$responseData = json_decode($geocodeResponseData, true);
-if($responseData['status']=='OK') {
-$latitude = isset($responseData['results'][0]['geometry']['location']['lat']) ? $responseData['results'][0]['geometry']['location']['lat'] : "";
-$longitude = isset($responseData['results'][0]['geometry']['location']['lng']) ? $responseData['results'][0]['geometry']['location']['lng'] : "";
-$formattedAddress = isset($responseData['results'][0]['formatted_address']) ? $responseData['results'][0]['formatted_address'] : "";
-if($latitude && $longitude && $formattedAddress) {
-$geocodeData = array();
-array_push(
-$geocodeData,
-$latitude,
-$longitude,
-$formattedAddress
-);
-return $geocodeData;
-} else {
-return false;
-}
-} else {
-echo "ERROR: {$responseData['status']}";
-return false;
-}
-}
+$name = $_POST['nombre'];
+$address = $_POST['address'];
+$referer = $_POST['referencia'];
+$lat = $_POST['lati'];
+$long = $_POST['longi'];
 
-$geocodeData = getGeocodeData($_POST['searchAddressd']);
-if($geocodeData){
-	$latitude = $geocodeData[0];
-	$longitude = $geocodeData[1];
-	$address = $geocodeData[2];
-	echo json_encode(array('latitudess' => $latitude, 'longitudess' => $longitude, 'direcciones' => $address));
-}      
-         
-?>
-   
+
+$dat = new DatosAdmin();
+$dat->nombre = $name;
+$dat->direccion = $address;
+$dat->referencia = $referer;
+$dat->latitud = $lat;
+$dat->longitud = $long;
+$dat->zoom = "14";
+$dat->guardar_sucursal_nuevo();
+echo json_encode(array('type' => 1, 'mensaje' => "listo"));
