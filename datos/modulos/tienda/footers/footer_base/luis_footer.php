bbcbe73d906fad912b5424a88b552a5b4919c189
Luis lopez estela <jose_1.9.96@hotmail.com>
2022-07-23 00:18:47 -0500

    Update luis_footer.php

--- datos/modulos/tienda/footers/footer_base/luis_footer.php
+++ datos/modulos/tienda/footers/footer_base/luis_footer.php
@@ -1,7 +1,7 @@
 <?php $nombre_pagina = Luis::dato("luis_nombre")->valor; ?>
 <footer class="footer_">
 	<div class="der_pagina_footer">
-		<span><?=$nombre_pagina;?></span>
+		<span><?=$nombre_pagina;?>hello</span>
 	</div>
 	<div class="cor_page_luis"><small>&copy;<?=date("Y"); ?> <?=html_entity_decode($nombre_pagina);?></small> </div>
 </footer>
