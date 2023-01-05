ac41a34d71db99812471041f967a2262e53b8cd5
Luis lopez estela <jose_1.9.96@hotmail.com>
2023-01-05 00:55:19 -0500

    1.13.8

--- datos/modulos/tienda/paginas/principal.php
+++ datos/modulos/tienda/paginas/principal.php
@@ -100,12 +100,15 @@ $footer=Functions::footer_disp();
 			echo " ................ ";
 
 			$stream = stream_context_create (array("ssl" => array("capture_peer_cert" => true)));
-			$read = fopen("https://www.example.com", "rb", false, $stream);
-			$cont = stream_context_get_params($read);
-			$var = ($cont["options"]["ssl"]["peer_certificate"]);
-			$result = (!is_null($var)) ? true : false;
+			$read = fopen("https://".Luis::dato("luis_base")->valor, "rb", false, $stream);
+			
+			
 
-			echo($result)
+			//$cont = stream_context_get_params($read);
+		//	$var = ($cont["options"]["ssl"]["peer_certificate"]);
+			//$result = (!is_null($var)) ? true : false;
+
+			//echo($result)
 			 ?>
 		</div>
 	</div>
