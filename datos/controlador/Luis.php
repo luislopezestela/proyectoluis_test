165053a4dd320b1cf70b44a8b415a9b8db0e1bdb
Luis lopez estela <jose_1.9.96@hotmail.com>
2023-01-07 00:02:20 -0500

    1.15.6

--- datos/controlador/Luis.php
+++ datos/controlador/Luis.php
@@ -564,9 +564,9 @@ class Luis {
 	public static function httpconf(){
 			if(Luis::ver_certificado(Luis::dato("luis_base")->valor)) {
 	    		if(isset($_SERVER['HTTPS']) != "on"){
-				    //$url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-				  //  header("Location: $url");
-				  //  exit;
+				    $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+				    header("Location: $url");
+				    exit;
 				}
 	    	}else{
 	    		$pageURLvalor = "http://";
