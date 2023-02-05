088a514cc4a697d3baeb8bdc74743e6fbaa86e37
Luis lopez estela <jose_1.9.96@hotmail.com>
2023-02-05 12:03:56 -0500

    s1

--- admin/datos/controlador/BaseDatos.php
+++ admin/datos/controlador/BaseDatos.php
@@ -1,41 +1,45 @@
 <?php
 class BaseDatos {
-  public static $db;
-  public static $con;
-  function __construct(){
-    $this->user="layshanetec";
-    $this->pass="layshanetecdata";
-    $this->host="localhost";
-  }
+	public static $db;
+	public static $con;
+	function __construct(){
+		$this->user="layshanetec";
+		$this->pass="layshanetecdata";
 
-  public static function basedatos_p(){
-    $datass = "layshane";
-    return $datass;
-  }
-  function conectar(){
-    $con = new mysqli($this->host,$this->user,$this->pass,BaseDatos::basedatos_p());
-    $con->query("SET NAMES utf8");
-    //$con->query("CREATE DATABASE IF NOT EXISTS " .BaseDatos::basedatos_p()." DEFAULT CHARACTER SET UTF8");
-    //mysqli_select_db($con, BaseDatos::basedatos_p());
-    if (mysqli_connect_errno()){
-      exit();
-    }
-    return $con;
-  }
-  public static function getCon(){
-    if(self::$con==null && self::$db==null){
-      self::$db = new BaseDatos();
-      self::$con = self::$db->conectar();
-    }
-    return self::$con;
-  }
+		//$this->user="root";
+		//$this->pass="";
+		$this->host="localhost";
+	}
+
+	public static function basedatos_p(){
+		//$datass = "restaurant";
+		$datass = "layshane";
+		return $datass;
+	}
+	function conectar(){
+		$con = new mysqli($this->host,$this->user,$this->pass,BaseDatos::basedatos_p());
+		$con->query("SET NAMES utf8");
+		//$con->query("CREATE DATABASE IF NOT EXISTS " .BaseDatos::basedatos_p()." DEFAULT CHARACTER SET UTF8");
+		//mysqli_select_db($con, BaseDatos::basedatos_p());
+		if (mysqli_connect_errno()){
+			exit();
+		}
+		return $con;
+	}
+	public static function getCon(){
+		if(self::$con==null && self::$db==null){
+			self::$db = new BaseDatos();
+			self::$con = self::$db->conectar();
+		}
+		return self::$con;
+	}
 
 
-  public static function creardatabase($nombre){
-    $con = new mysqli($this->host,$this->user,$this->pass);
-    $con->query("SET NAMES utf8");
-    $con->query("CREATE DATABASE IF NOT EXISTS " .$nombre." DEFAULT CHARACTER SET UTF8");
-    mysqli_select_db($con, $nombre);
-    return $con;
-  }
+	public static function creardatabase($nombre){
+		$con = new mysqli($this->host,$this->user,$this->pass);
+		$con->query("SET NAMES utf8");
+		$con->query("CREATE DATABASE IF NOT EXISTS " .$nombre." DEFAULT CHARACTER SET UTF8");
+		mysqli_select_db($con, $nombre);
+		return $con;
+	}
 }
